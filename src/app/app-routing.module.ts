bc4611bc299902d57e2cf71cfe4c0cfa7858c70a
Abid <abid.m.abdelaziz@gmail.com>
2019-12-11 15:16:08 -0500

    Account dashboard view skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountsComponent } from "./components/accounts/accounts.component";
 import { LandingPageComponent } from "./components/landing-page/landing-page.component"
-
+import { AccountviewComponent } from "./components/accountview/accountview.component"
 
 const routes: Routes = [
   { path: 'accounts', component: AccountsComponent },
+  { path: 'accounttype', component: AccountviewComponent },
   { path: '', component: LandingPageComponent },
 
 
