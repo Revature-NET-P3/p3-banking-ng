2f9bf922b95fad71ea7d96d1ce9cd5ff8c25eaee
Abid <abid.m.abdelaziz@gmail.com>
2019-12-11 11:01:43 -0500

    First Draft layout skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountsComponent } from "./components/accounts/accounts.component";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component"
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accounts', component: AccountsComponent },
+  { path: '', component: LandingPageComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentareaComponent } from './components/contentarea/contentarea.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContentareaComponent,
+    AccountsComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
