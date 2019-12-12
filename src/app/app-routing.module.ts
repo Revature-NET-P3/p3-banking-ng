11b03b91e3931d9f659b89dac17b06a77a4e2b93
Zander <steamwings@users.noreply.github.com>
2019-12-12 13:40:59 -0500

    Fix formatting on app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AccountsComponent } from "./components/accounts/accounts.component";
-import { LandingPageComponent } from "./components/landing-page/landing-page.component"
-import { AccountViewComponent } from "./components/account-view/account-view.component"
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AccountViewComponent } from './components/account-view/account-view.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 const routes: Routes = [
