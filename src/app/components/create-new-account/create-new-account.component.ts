db46f02fa54ed1f4c8a0cf9efcbc92e05b9691a5
JnChauhan <jacros33@gmail.com>
2019-12-16 15:54:40 -0500

    list of accounts hardcoded

--- src/app/components/create-new-account/create-new-account.component.ts
+++ src/app/components/create-new-account/create-new-account.component.ts
@@ -3,13 +3,18 @@ import { FormControl } from '@angular/forms';
 import { AccountsService } from 'src/app/services/accounts.service';
 import { Account, AccountType } from '../../models/account';
 import { ApiService } from 'src/app/services/api.service';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
+
 
 @Component({
   selector: 'app-create-new-account',
   templateUrl: './create-new-account.component.html',
   styleUrls: ['./create-new-account.component.css'],
 
-  providers: [AccountsService]
+  providers: [
+    { provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } },
+    AccountsService, ApiService
+  ]
   
 })
 export class CreateNewAccountComponent implements OnInit {
@@ -17,12 +22,18 @@ export class CreateNewAccountComponent implements OnInit {
   newAccount: Account = null;
   openCreate: boolean = false;
   filterOptions = AccountType.AllNames();
+  selectedAccountType:string = "Select an Account Type";
+
   //accountType:AccountType;
   constructor(private apiSvc: ApiService) { 
     //currentAccount: Account = null;
   }
 
   ngOnInit() {
+  }
+
+  setAccountType(typeAcc: string){
+    this.selectedAccountType = typeAcc;
   }
 
   showSelectedType(accType: AccountType){
@@ -36,7 +47,20 @@ export class CreateNewAccountComponent implements OnInit {
     this.newAccount.balance = null;
     this.newAccount.createDate = null;
     this.newAccount.isClosed = false;
-    
+    if(this.newAccount.userId !=null)
+    {
+      if(this.newAccount.accountTypeId != 0)
+      {
+        if(this.newAccount.balance >=0){
+
+        }
+
+      }
+    }
+    else
+    {
+
+    }
     //this.apiSvc
     
   }
