9269e700754318bb6b2cf6cc87f25cb1eba74d01
JnChauhan <jacros33@gmail.com>
2019-12-16 19:23:17 -0500

    500 error code coming from api when create

--- src/app/components/create-new-account/create-new-account.component.ts
+++ src/app/components/create-new-account/create-new-account.component.ts
@@ -7,6 +7,7 @@ import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 import { environment } from 'src/environments/environment';
 import { formatDate } from '@angular/common';
 
+
 @Component({
   selector: 'app-create-new-account',
   templateUrl: './create-new-account.component.html',
@@ -20,6 +21,7 @@ import { formatDate } from '@angular/common';
 })
 export class CreateNewAccountComponent implements OnInit {
 
+  private locale = "en-US";
   newAccount: Account = null;
   openCreate: boolean = false;
   filterOptions = AccountType.AllNames();
@@ -46,10 +48,14 @@ export class CreateNewAccountComponent implements OnInit {
   }
 
   createAccount(type:string){
-
+    // |date:'fullDate'
+    
     //var currentTime = formatDate(Date.now(), 'yyyy-MM-dd', 'en-US', '+0500') + 'T' + formatDate(Date.now(), 'hh:mm:ss', 'en-US', '+0500') + '.00'
+    //currentTime : string = new Date().toISOString();
+    //var currentTime = formatDate(Date.now(), 'yyyy-MM-dd', 'en-US', '+0500');
+    var currentTime = new Date();
     this.newAccount = new Account({
-        id: null,
+        id: 1,
         userId: 1,
         accountTypeId: AccountType[type],
         balance: 0,
@@ -77,6 +83,11 @@ export class CreateNewAccountComponent implements OnInit {
       {this.creatingAccount=false; /*this.postedAccount = postResponse as Account;*/ console.log(postResponse)});
   }
 
+
+  //looks for string value
+  formatDate(date: string) : string {
+    return formatDate(date, 'medium', this.locale);
+  }
 
 
 }
