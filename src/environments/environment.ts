9c03ae9bfa9682c803f2c378b6ae4b98c4586c8f
Jake Hills <jhills2125@gmail.com>
2019-12-17 14:04:18 -0500

    had to re-add the apiURL for the deployed app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   title: "P3Bank (Debug)",
   production: false,
-  //apiUrl: "https://p3api.azurewebsites.net",
-  apiUrl: "https://localhost:44357",
+  apiUrl: "https://p3api.azurewebsites.net",
+  //apiUrl: "https://localhost:44357",
   auth0Token:"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5qYzROMFZETVRZd05UYzNSVUZCTUVVMFJrRXhOakF4TWpORk1FRTROVEl5UmpVeE5EUTJSUSJ9.eyJpc3MiOiJodHRwczovL3AzLWJhbmtpbmcuYXV0aDAuY29tLyIsInN1YiI6ImI1cTlpZ21DMzhjUVBxSjdVNFM1Vk9mdGVsbVBVQ016QGNsaWVudHMiLCJhdWQiOiJwMy1iYW5raW5nIiwiaWF0IjoxNTc2NTQ4MDU3LCJleHAiOjE1NzY2MzQ0NTcsImF6cCI6ImI1cTlpZ21DMzhjUVBxSjdVNFM1Vk9mdGVsbVBVQ016IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.i5LMF4Vj8kpKabSSJG4g0ZulgPNCWZDS6FGOfy72irxfuMKTYQ8OParnpX06UpaE2vK-sDfTSJ5IE66i58EMZVSlTkXGSrstjf1odkvb1Q_PGx7LT6lmOXp_4jZgyNG1dyH-lWqc_yh7xhe5b_qR9b_Ecn5QatHrykiuKuGAlgRdCgLksJC_nS9Q6e2r1j41pn5W5Yul63htiCcE8xorO4WAGysDfRty_SmcKAuY5Z9OZKVw9pK5gF00TAuFKXCul4Fx_nW47HjK_yim1U5Is2cX28w9m9Tv3ZoF7-OMuUHkkvzrHp4upRdaz4Voze0AAHG76NmfH9_4dB9Kgc06uw"
 };
 
